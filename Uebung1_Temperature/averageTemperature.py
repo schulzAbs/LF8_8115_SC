cd4872ac051a1a6bbebc1cde9156de74d170311f
Wolfgang Schulz <SC@musterschule.schule.paedml>
2021-11-23 17:27:59 +0100

    Final Temperature

---
+++ Uebung1_Temperature/averageTemperature.py
@@ -0,0 +1,15 @@
+
+#averageTemperature
+
+temp1 = float(input("Please enter your first temperature  measurement:"))
+temp2 = float(input("Please enter your second temperature measurement:"))
+temp3 = float(input("Please enter your third temperature  measurement:"))
+
+tempav = (temp1 + temp2 + temp3)/3
+
+#temps = [temp1, temp2, temp3]
+#tempav = (sum(temps))/3
+
+print(f"Your average temperature is {tempav}")
+
+
